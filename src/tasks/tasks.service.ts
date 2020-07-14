7dea374654b68f558182b93728284a449faa045c
BaoPham92 <baopham92@yahoo.com>
2020-07-13 21:36:43 -0400

    Created Task Get req

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task.model';
 
 @Injectable()
 export class TasksService {
+    private tasks: Task[] = [];
 
-    // * GET ALL TASKS
-    getAllTasks() {
-        
+    getAllTasks(): Task[] {
+        return this.tasks
     }
 }
